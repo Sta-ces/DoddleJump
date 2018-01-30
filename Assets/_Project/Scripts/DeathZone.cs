c6993a38e40cefeaeee189f9b648d201c2d93922
Staces <staescedric@gmail.com>
2018-01-30 17:28:18 +0100

    First Proto

--- Assets/_Project/Scripts/DeathZone.cs
+++ Assets/_Project/Scripts/DeathZone.cs
@@ -1,14 +1,17 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(BoxCollider2D))]
 public class DeathZone : MonoBehaviour {
+
+    public Text m_GameOverText;
+
 
     private void Awake()
     {
         m_cameraScreen = Calcul.ScreenSize(Camera.main);
         GetComponent<BoxCollider2D>().isTrigger = true;
-        m_player = FindObjectOfType<PlayerController2D>();
-        m_plateform = FindObjectOfType<PlateformBouncing>();
+        m_GameOverText.text = "";
     }
 
     private void Start()
@@ -16,23 +19,25 @@ public class DeathZone : MonoBehaviour {
         transform.localScale = new Vector2(m_cameraScreen.x * 2 + transform.lossyScale.x, 1f);
     }
 
-    private void Update()
+    private void LateUpdate()
     {
         transform.position = new Vector2(0f, ((-m_cameraScreen.y * .5f) - (transform.lossyScale.y * 2)) + Camera.main.transform.position.y);
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject == m_player.gameObject)
+        if (other.gameObject.GetComponent<PlayerController2D>() != null)
+        {
             print("GAME OVER!");
-        else if(other.gameObject == m_plateform.gameObject)
+            m_GameOverText.text = "Game Over";
+            other.gameObject.SetActive(false);
+        }
+        else if(other.gameObject.GetComponent<PlateformBouncing>() != null)
+        {
             print("PLATFORM DESTROY!");
-
-        Destroy(other.gameObject);
-
+            Destroy(other.gameObject);
+        }
     }
 
     private Vector3 m_cameraScreen;
-    private PlayerController2D m_player;
-    private PlateformBouncing m_plateform;
 }
