b5bef9621347dee230886cb2c5b6db0d037cc62a
Staces <staescedric@gmail.com>
2018-01-30 15:00:42 +0100

    Death Zone is in place

---
+++ Assets/_Project/Scripts/DeathZone.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class DeathZone : MonoBehaviour {
+
+
+
+    private void Awake()
+    {
+        m_cameraScreen = Calcul.ScreenSize(Camera.main);
+        GetComponent<BoxCollider2D>().isTrigger = true;
+        transform.localScale = new Vector2(m_cameraScreen.x *2 + transform.lossyScale.x, 1f);
+    }
+
+    private void Update()
+    {
+        transform.position = new Vector2(0f, ((-m_cameraScreen.y * .5f) - (transform.lossyScale.y * 2)) + Camera.main.transform.position.y);
+    }
+
+
+    private Vector3 m_cameraScreen;
+}
